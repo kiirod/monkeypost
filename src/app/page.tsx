4b1c645cb87dab08837ff700b9e4d0ac04efd284
kiirod <py@astrarune.com>
2026-04-30 20:09:22 +0100

    Add Home component for upcoming Monkeypost site

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen text-white text-center px-4">
+      <h1 className="text-4xl md:text-6xl font-bold mb-4">
+        Monkeypost is coming soon.
+      </h1>
+
+      <p className="text-lg md:text-xl opacity-50 font-light">
+        Not affiliated with the developers of Monkeytype.
+      </p>
+    </main>
+  );
+}
