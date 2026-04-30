9cf1096d63024d143cc69657022365e153968542
kiirod <py@astrarune.com>
2026-04-30 20:20:00 +0100

    Add not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+export default function NotFound() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen text-white text-center px-4">
+      <h1 className="text-4xl md:text-6xl font-bold mb-4">
+        Page Not Found
+      </h1>
+
+      <p className="text-lg md:text-xl opacity-50 font-light">
+        Made a wrong turn?
+      </p>
+    </main>
+  );
+}
