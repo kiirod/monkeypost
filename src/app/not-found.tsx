76063cd69b6459a4d7ce810826b6ef97410c8cb4
kiirod <py@astrarune.com>
2026-05-01 22:34:04 +0100

    revert back to old not-found.tsx so vercel is happy again and lets me do changes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,27 +7,6 @@ export default function NotFound() {
       <p className="text-lg md:text-xl opacity-50 font-light">
         Made a wrong turn?
       </p>
-
-      
-        href="/"
-        className="mt-10 flex items-center gap-3 px-6 py-3 rounded-xl text-sm font-medium text-white border border-white border-opacity-10 bg-white bg-opacity-10 hover:bg-opacity-20 transition-all duration-200"
-      >
-        <svg
-          viewBox="0 0 16 16"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-        >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M8 0L0 6V8H1V15H4V10H7V15H15V8H16V6L14 4.5V1H11V2.25L8 0ZM9 10H12V13H9V10Z"
-            fill="white"
-          />
-        </svg>
-        Go Home
-      </a>
     </main>
   );
 }
